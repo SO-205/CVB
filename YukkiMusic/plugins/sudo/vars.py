c32406ad50bc2363f65f8887f3c00d03238550f5
SO-205 <102315435+SO-205@users.noreply.github.com>
2022-03-24 20:25:48 +0200

    Update vars.py

--- YukkiMusic/plugins/sudo/vars.py
+++ YukkiMusic/plugins/sudo/vars.py
@@ -28,7 +28,7 @@ async def varsFunc(client, message):
     )
     v_limit = await get_video_limit()
     bot_name = config.MUSIC_BOT_NAME
-    up_r = f"[Repo]({config.UPSTREAM_REPO})"
+    up_r = f"[source lura](https://t.me/so_alfaa)"
     up_b = config.UPSTREAM_BRANCH
     auto_leave = config.AUTO_LEAVE_ASSISTANT_TIME
     yt_sleep = config.YOUTUBE_DOWNLOAD_EDIT_SLEEP
